6f14f987b08e959b5bab65407ceb60cd3bc16251
Nahuel-36 <nahuelvaldeskendo@gmail.com>
2025-08-30 20:06:30 +0000

    creo que el ultimo  retunr no va

---
+++ corregido/app2.py
@@ -0,0 +1,137 @@
+#instalar flask
+from flask import Flask, jsonify, request
+import json
+import os
+
+app = Flask(__name__)
+
+DATA_FILE = "vuelos.json"
+
+# Crea una función cargar_datos() que:
+def cargar_datos():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+# Función auxiliar: guardar datos  
+def guardar_datos(datos):
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(datos, f, indent=4, ensure_ascii=False)
+
+@app.route("/", methods=["GET"])
+def inicio():
+    #jsonify tiene un conjunto como parametro({})
+    return jsonify({"mensaje":"API para vuelos"})
+
+@app.route("/api/vuelos", methods=["GET"])
+def listar_vuelos():
+    datos = cargar_datos()
+    for dato in datos:
+        dato["destino"] = dato["destino"].title()
+    return jsonify(datos)
+
+@app.route("/api/vuelos/<int:vuelo_id>", methods=["GET"])
+def obtener_vuelo(vuelo_id):
+    datos = cargar_datos()
+    for vuelo in datos:
+        vuelo["destino"] = vuelo["destino"].title()
+        if vuelo["id"] == vuelo_id:
+            return jsonify(vuelo) #vuelo?
+    return jsonify({"error":"Vuelo no encontrado"}), 404
+
+@app.route("/api/vuelos", methods=["POST"])
+def agregar_vuelo():
+    #pedir una request? con json que es una funciin, request.get_json()
+    nuevo_vuelo = request.get_json()
+    #validar con un getter
+    if not nuevo_vuelo.get("destino"):
+        #retornar en jsonfy
+        return jsonify({"error": "El campo 'destino' es obligatorio"}), 400
+    #usar los datos
+    #se va a cargar en la ultima posición
+    datos = cargar_datos()
+    if datos:
+        nuevo_vuelo["id"] = datos[-1]["id"] + 1
+    else:
+        nuevo_vuelo["id"] = 1
+    #nuevo_vuelo["id"] = datos[-1]["id"] + 1 if datos else 1
+    
+    if "capacidad" not in nuevo_vuelo or not nuevo_vuelo["capacidad"]:
+        nuevo_vuelo["capacidad"] = 100
+    else:
+        nuevo_vuelo["capacidad"] = int(nuevo_vuelo["capacidad"])
+
+    if "vendidos" not in nuevo_vuelo or not nuevo_vuelo["vendidos"]:
+        nuevo_vuelo["vendidos"] = 0
+    else:
+        nuevo_vuelo["vendidos"] = int(nuevo_vuelo["vendidos"])
+    
+    #pruebo otra forma con .get()
+    #nuevo_vuelo["capacidad"] = int(nuevo_vuelo.get("capacidad", 100))
+    #nuevo_vuelo["vendidos"] = int(nuevo_vuelo.get("vendidos", 0))
+
+    nuevo_vuelo["destino"] = nuevo_vuelo["destino"].lower()
+    
+    #agregar a la lista
+    datos.append(nuevo_vuelo)
+    guardar_datos(datos)
+    return jsonify(nuevo_vuelo), 201
+
+# Consigna 5:
+#actualizar por id
+@app.route("/api/vuelos/<int:vuelo_id>", methods=["PUT"])
+def actualizar_vuelo(vuelo_id):
+    vuelo_dato = request.get_json()
+    datos = cargar_datos()
+    
+    #buscar los id
+    for vuelo in datos:
+        if vuelo["id"] == vuelo_id:
+            vuelo.update({ #lleva coma porque es un diccionario
+                "destino" : vuelo_dato.get("destino", vuelo["destino"]),
+                "capacidad" : int(vuelo_dato.get("capacidad", vuelo["capacidad"])),
+                "vendidos" : int(vuelo_dato.get("vendidos", vuelo["vendidos"]))
+            })
+            vuelo["destino"] = vuelo["destino"].title()
+            guardar_datos(datos)
+            return jsonify(vuelo)
+    return jsonify({"error": "Vuelo no encontrado"}), 404
+
+# Consigna 6:
+@app.route("/api/vuelos/<int:vuelo_id>", methods=["DELETE"])
+def eliminar_vuelo(vuelo_id):
+    datos = cargar_datos()
+    nuevos_datos = [vuelo for vuelo in datos if vuelo["id"] != vuelo_id]
+    
+    if len(nuevos_datos) == len(datos):
+        return jsonify({"error": "Vuelo no encontrado"}), 404
+    elif len(nuevos_datos) != len(datos):
+        guardar_datos(nuevos_datos)
+        
+    return jsonify({"mensaje": f"Vuelo {vuelo_id} eliminado correctamente"})
+
+
+@app.route("/api/vender", methods=["POST"])
+def vender_vuelo():
+    #obtener datos
+    datos = request.get_json()
+    vuelo_id = datos.get("id")
+    cargar_datos(datos)
+    for vuelo in datos:
+        if vuelo["id"] == vuelo_id:
+            #se usa un if anidado para ver si hay espacio
+            if vuelo["vendidos"] <= vuelo["capacidad"]:
+                vuelo["vendidos"] += 1
+                #recien acá se guarda y se retorna si cumplió las condiciones
+                guardar_datos(datos)
+                return jsonify(vuelo)
+            elif vuelo["vendidos"] >= vuelo["capacidad"]:
+                return jsonify({"error": "Vuelo completo"}), 400
+    return jsonify({"error": "Vuelo no encontrado"}), 404
+    #return jsonify(vuelo)
+
+if __name__ == "__main__":
+    if not os.path.exists(DATA_FILE):
+        guardar_datos([])
+    app.run(debug=True)
